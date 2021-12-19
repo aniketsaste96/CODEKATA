044f0501c1671d689eeae0641448e916004fd2af
aniketsaste96 <90027213+aniketsaste96@users.noreply.github.com>
2021-12-19 10:11:47 +0530

    Create Reverse_words_string.js

---
+++ Reverse_words_string.js
@@ -0,0 +1,14 @@
+// Sample Input :
+// guvi geek
+// Sample Output :
+// ivug keeg
+
+
+let data= userInput[0]
+
+console.log(data.split("").reverse().join("").split(" ").reverse().join(" "))
+
+// Output:
+// ivug keeg
+// Execution Time:
+// 0.072s
