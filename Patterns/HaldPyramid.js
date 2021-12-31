6d450f60e71115ad960bbcaa63ec333e230c044e
aniketsaste96 <90027213+aniketsaste96@users.noreply.github.com>
2021-12-31 18:27:51 +0530

    Create HaldPyramid.js

---
+++ Patterns/HaldPyramid.js
@@ -0,0 +1,19 @@
+*
+*  *
+*  *  *
+*  *  *  *
+*  *  *  *  *
+  
+ // ---------------------------------
+  
+    let num = userInput[0]
+  let result = ""
+for(let row =1;row<=5;row++){
+    //for every row run the col
+    for(let col=1;col<=row;col++){
+        result+="*  "
+    }
+    //when one row is printed we need new line
+     result+="\n"
+}
+console.log(result.replace(/\s+$/gm, ""))
