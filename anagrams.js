0d10eeb0d7f43088b6e0e7069fc9348b1e03a738
aniketsaste96 <90027213+aniketsaste96@users.noreply.github.com>
2022-01-03 22:19:41 +0530

    Create anagrams.js

---
+++ anagrams.js
@@ -0,0 +1,28 @@
+ Joseph was going through topic of strings. He learnt about anagrams. But due to some circumstances he forget ,now he hired you to help him in completing the work.Your task is to tell whether the two given strings are anagrams
+
+Input Description:
+The first line of the input is a string N, the second line of the input is a string M
+
+Output Description:
+Compare the two string input N and M. Print 1 if they are anagram else print 0.
+
+Sample Input :
+abcd
+cdab
+Sample Output :
+1
+
+//-------------------------------------
+let data = userInput[0].split("");
+let data1 = userInput[1].split("");
+
+ if(data.sort().join("")===data1.sort().join("")){
+     console.log("1")
+ }else{
+     console.log("0")
+ }
+//--------------------------------------
+Output:
+1
+Execution Time:
+0.072s
