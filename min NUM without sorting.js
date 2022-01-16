078929124c4564eac0a88b402cb355b8032ee1d1
aniketsaste96 <90027213+aniketsaste96@users.noreply.github.com>
2022-01-16 12:42:32 +0530

    Create min NUM without sorting.js

---
+++ min NUM without sorting.js
@@ -0,0 +1,16 @@
+Output Description:
+Print the minimum element
+
+Sample Input :
+5
+3 4 9 1 6
+Sample Output :
+1
+
+//-------------------------------------
+let n =userInput[0];
+  let data = userInput[1].split(' ');
+
+
+let min=Math.min(...data);
+console.log(min);
