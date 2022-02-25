951aac8571e8e0fb3c823fedd0509e7d913b09ed
aniketsaste96 <90027213+aniketsaste96@users.noreply.github.com>
2022-02-25 23:13:48 +0530

    Create Print numbers between given number.js

---
+++ Numbers/Print numbers between given number.js
@@ -0,0 +1,14 @@
+Output Description:
+print the numbers between this two numbers
+
+Sample Input :
+5 8
+Sample Output :
+6 7
+
+//----------------
+let result ="";
+    for(let i = A+1;i<B;i++){
+      result+=[i]+" "
+    }
+console.log(result.trim())
