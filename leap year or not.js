7ac4a285ab5ec594c55f4e158d33105821bfe891
aniketsaste96 <90027213+aniketsaste96@users.noreply.github.com>
2022-02-25 23:14:20 +0530

    Create leap year or not.js

---
+++ leap year or not.js
@@ -0,0 +1,19 @@
+Given a year, find whether leap year or not?
+
+Input Description:
+Given a 4 digit number
+
+Output Description:
+Print leap year or not a leap year
+
+Sample Input :
+1996
+Sample Output :
+leap year
+//---------------
+ let data = userInput[0];
+if(data%4===0){
+    console.log("leap year")
+}else{
+    console.log("not a leap year")
+}
